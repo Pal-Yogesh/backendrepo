5f25f773be3fbf20a410450da6a5f4d15765562e
Yogesh Pal <yogeshpal5049@gmail.com>
2025-11-03 16:36:03 +0530

    done

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
 
   app.enableCors({
     origin: [
-      'https://frontendrepo-three.vercel.app',
+      'https://frontendrepo-opal.vercel.app',
       'http://localhost:3000'
     ],
     credentials: true,
